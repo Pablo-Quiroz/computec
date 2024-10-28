93bdb200ec225c5600d8a7f0567ac210bcf956a0
Pablo Quiroz <pabloquiroz262003@gmail.com>
2024-10-27 23:19:30 -0500

    creacion e inicio de proyecto con spring, maven y dependecias

---
+++ src/main/java/com/computec/computec/ComputecApplication.java
@@ -0,0 +1,13 @@
+package com.computec.computec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computec/computec/ComputecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computec.computec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
